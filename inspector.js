1a2f13e542c8d4c2ad527d4c51881a15e50dc65e
mikeplavsky <Mike.Plavsky@quest.com>
2010-12-17 18:39:17 +0300

    init commit

---
+++ inspector.js
@@ -0,0 +1,9 @@
+if ( $( 'link' ).filter( function() { return $(this).attr( 'href' ).match( 'fitnesse' )  } ).length && !$( '#test-summary' ).length ) {
+
+	console.log( 'Started Fitnesse Variables' );	
+	
+};
+
+
+
+
