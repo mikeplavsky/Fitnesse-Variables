ee36112a0d94f3d9d03b513d0304c573481a52f4
mikeplavsky <Mike.Plavsky@quest.com>
2010-12-23 19:37:48 +0300

    Added core/variables.js into FitnesseRoot

---
+++ tests/FitNesseRoot/files/core/variables.js
@@ -0,0 +1,48 @@
+function fv_variables(url) {
+
+	var vars = {};
+	var nums = 1;
+	
+	function getVars(url) {
+	
+		nums += 1;	
+		
+		$.get( url, function (res) {
+			
+			nums -= 1;
+			
+			var m = jQuery( res ).find( '#pageContentId' ).text().match(  /^!define [\w\.]+ +(?:(?:\{[^}]*\})|(?:\([^)]*\))|(?:\[[^\]]*\]))/gm );		
+			
+			m && $.each( m, function () {				
+				
+				var v = this.match( /^!define ([\w\.]+) +([\{\(\[])([\s\S]*)[\}\)\]]/m );
+				
+				vars[ v[1] ] || (vars[ v[1] ] = []);
+				vars[ v[1] ].push( { url: url, value: v[3] } );
+								
+			});
+			
+			if (nums === 1) {
+				$( '#fv-found-variables' ).trigger( 'message', vars );
+			}
+			
+		});
+	}
+	
+	function walkUp(url) {
+	
+		var re = /\.\w*$/;
+	
+		if (! url.match( re ) ) {
+			return;
+		}
+		
+		getVars( url + '?edit' );
+		url =  url.replace( re, '' );		
+		
+		walkUp( url );	
+	}	
+
+	walkUp( url );	
+
+}
