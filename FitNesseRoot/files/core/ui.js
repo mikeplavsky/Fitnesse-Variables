a23ad64bd5307ddeb29f2564fc297fa28388a964
mikeplavsky <mike.plavsky@quest.com>
2015-04-09 10:48:12 +0300

    click outside dlg closes it

--- FitNesseRoot/files/core/ui.js
+++ FitNesseRoot/files/core/ui.js
@@ -1,34 +1,61 @@
 function fv_ui() {
 
-	$( 'body' ).append( '<div id="fv-found-variables">' );
+    $(document).click(
+        function(event) {
+
+            if (!$(event.target)
+                .closest(
+                    "div[id*='fv'], div[class*='fv']")
+                .length) {
+
+                if ($("#fv-variables-dlg")
+                    .is(":visible")) {
+
+                    $('#fv-variables-dlg')
+                        .hide();
+
+                }
+            }
+        });
+
+    $('body').append('<div id="fv-found-variables">');
     $("#fv-found-variables").hide();
-    
-    $( '#fv-found-variables' ).bind( 'message', function (ev,data) {	
-		
-		var vars = create_array(data).sort();		
-		
-		if (!vars.length) {
-			return;						
-		}		
-		
-		$(this).show();
-		$(this).text( 'Variables(' + vars.length + ')' );
-        
-        $( '#fv-found-variables' ).click( function () {       
-            
-            $('#fv-variables-dlg').remove();       
-            $( 'body' ).append( $('<div id="fv-variables-dlg">').hide() );
-       
-            $( '#fv-variables-dlg' ).append( $( fitnesse.variables.main( {variables: vars, data: data} )) );	        
-            $( '#fv-variables-dlg' ).append( $('<div id="fv-variables-hide">Close</div>'));        
-       
-            $( '#fv-variables-hide' ).click( function() {        
-                $( '#fv-variables-dlg' ).hide();
+
+    $('#fv-found-variables').bind('message', function(ev, data) {
+
+        var vars = create_array(data).sort();
+
+        if (!vars.length) {
+            return;
+        }
+
+        $(this).show();
+        $(this).text('Variables(' + vars.length + ')');
+
+        $('#fv-found-variables').click(function() {
+
+            $('#fv-variables-dlg').remove();
+
+            $('body').append($(
+                '<div id="fv-variables-dlg">' +
+                '<div id="fv-variables-hide">Close</div>' +
+                '<div id="fv-variables-box">' +
+                '</div></div>'
+            ).hide());
+
+            $('#fv-variables-box')
+                .append($(fitnesse.variables.main({
+                    variables: vars,
+                    data: data
+                })));
+
+            $('#fv-variables-hide').click(function() {
+                $('#fv-variables-dlg').hide();
             });
-            
-            $( '#fv-variables-dlg' ).show();
-       
-        });   
-        
-	});
+
+            $('#fv-variables-dlg').show();
+
+        });
+
+    });
 }
